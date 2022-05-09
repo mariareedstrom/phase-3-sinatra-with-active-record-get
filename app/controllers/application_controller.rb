d92ea6918cc043a9fc0f854585ef476c8fa374d5
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-05-09 12:12:57 -0500

    commit tree 236361b1faa29b65660141923e018d35fd2a81e0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json
 
-  get '/' do
-    { message: "Hello world" }.to_json
+'
+  get '/games' do
+    #get data from database
+    games = Game.all.order(:title).limit(10)
+    #return JSON response with array of game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    #look up game in db using id
+    game = Game.find(params[:id])
+    # send JSON response of game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
